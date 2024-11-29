26666faa33b2b22d5c6706311c7c3c3bd626f2b0
shaiksanda <shaiksandanibasha7@gmail.com>
2024-11-29 16:40:12 +0530

    added Some Features

--- src/Components/Todo/index.js
+++ src/Components/Todo/index.js
@@ -1,6 +1,7 @@
 import { useState,useEffect} from 'react';
 import Calendar from 'react-calendar';
 import Cookies from 'js-cookie';
+import { Oval } from 'react-loader-spinner'
 import { FcFilledFilter } from "react-icons/fc";
 
 import 'react-calendar/dist/Calendar.css';
@@ -12,15 +13,48 @@ const Todo = () => {
   const [data,setData]=useState([])
 
   const [tag, setTag] = useState('');
+  const [priority, setPriority] = useState('');
   const [todo, setTodo] = useState('');
   const [msg,setMsg]=useState("")
   const [showMsg,setShowMsg] = useState(false)
 
-  const [priority, setPriority] = useState('');
+  const [filterTag, setFilterTag] = useState('');
+  const [filterPriority, setFilterPriority] = useState('');
+  const [search, setSearch] = useState('');
+  const [status, setStatus] = useState('');
+  const [isLoading, setIsLoading] = useState(true)
+  
+  
+
+  
+
+  const handleFilterTag=(event)=>{
+    setFilterTag(event.target.value)
+  }
+
+  const handleFilterPriority=(event)=>{
+    setFilterPriority(event.target.value)
+  }
+
+  const handleSearch=(event)=>{
+    setSearch(event.target.value)
+  }
+
+  const handleStatus=(event)=>{
+    setStatus(event.target.value)
+  }
+
+  const handleRemoveFilters=()=>{
+    setFilterTag("")
+    setFilterPriority("")
+    setSearch("")
+    setStatus("")
+  }
 
   useEffect(()=>{
     const getTodos=async()=>{
-      const url = `https://todos-backend-d9im.onrender.com/todos?selectedDate=${selectedDate.toISOString()}`;
+      setIsLoading(true)
+      const url = `https://todos-backend-d9im.onrender.com/todos?selectedDate=${selectedDate.toISOString()}&tag=${filterTag}&priority=${filterPriority}&status=${status}`;
       const options = {
         method:"GET",
         headers: {
@@ -32,15 +66,17 @@ const Todo = () => {
       const jsonData=await response.json()
       if (response.ok){
         setData(jsonData)
+        setIsLoading(false)
       }
       else{
         console.log("Error in fetching data",response.statusText)
+        setIsLoading(false)
       }
       
     }
 
     getTodos()
-  },[selectedDate])
+  },[selectedDate,filterPriority,filterTag,status])
 
   const onSubmitSuccess=(data)=>{
         setMsg(data.message)
@@ -74,19 +110,27 @@ const Todo = () => {
     };
   
     try {
-      const response = await fetch(url, options);
-      const data = await response.json();
-      if (response.ok) {
-        onSubmitSuccess(data)
+      if(tag && priority && todo) { 
+        const response = await fetch(url, options);
+        const data = await response.json();
+        if (response.ok) {
+          onSubmitSuccess(data)
+    
+        } else {
+          setMsg(data.message)
+          setShowMsg(true)
   
-      } else {
-        setMsg(data.message)
-        setShowMsg(true)
-
-        setTimeout(() => {
-          setShowMsg(false);
-        }, 5000);
+          setTimeout(() => {
+            setShowMsg(false);
+          }, 5000);
+      }
+     
+      
        
+      }
+      else{
+        setMsg("Please fill out all required fields!")
+        setShowMsg(true)
       }
     } catch (error) {
       console.error('Error adding task:', error);
@@ -122,6 +166,27 @@ const Todo = () => {
   const getRandomColor = () => {
       return colors[Math.floor(Math.random() * colors.length)];
     };
+
+    const handleDeleteTodo=async(id)=>{
+      const url=`https://todos-backend-d9im.onrender.com/todos/${id}`
+      const options={
+        method:"DELETE",
+        headers: {
+          'content-type': 'application/json',
+          'Authorization': `Bearer ${Cookies.get('jwt_token')}`, // Authorization header
+        },
+      }
+      const response=await fetch(url,options)
+      if (response.ok){
+        console.log("Todo deleted successfully")
+      }
+      else{
+        console.log("Failed to Delete Todo")
+      }
+    }
+    
+
+    const filteredData=data.filter((each)=>each.todo.toLowerCase().includes(search))
   
   return (
     <div className="todos-bg-container">
@@ -154,6 +219,7 @@ const Todo = () => {
                 style={{ color: 'black' }}
                 id="tag"
               >
+                <option value="default" hidden>Select One</option>
               <option value="Work">Work</option>
               <option value="Education">Education</option>
               <option value="Health">Health</option>
@@ -173,9 +239,11 @@ const Todo = () => {
                 className='input-element'
                 style={{ color: 'black' }}
             > 
+                <option value="default" hidden>Select One</option>
                 <option value="low">low</option>
                 <option value="medium">medium</option>
                 <option value="high">high</option>
+                
             </select>
 
 
@@ -187,22 +255,92 @@ const Todo = () => {
         </div>
      </div>
 
-     <h1 className="fetch-todos-heading">Fetching todos for the selected date: <span className="formatted-date-heading">{formattedDate}</span> from the server</h1>
+     <h1 className="fetch-todos-heading">Fetching todos for: <span className="formatted-date-heading">{formattedDate}</span></h1>
+     <div className="spinner-container">
+     <div className="spinner-container">
+      {isLoading && <Oval
+        color="green"
+        height={50}
+        width={50}
+        ariaLabel="loading"
+      />}
+    </div>
+    </div>
       <div className='todo-bottom-container'>
         <div>
-          <h1>Filter</h1>
-          <FcFilledFilter />
+          <h1 className='filter-heading'>Use Filters to Organize Your Data <FcFilledFilter size={"30px"} /></h1>
+          
+          <div className='filters-container'>
+            <input onChange={handleSearch} value={search} type="search" placeholder='Search...' className='input-element' />
+          <label htmlFor='filterTag' className='label'>TAG</label>
+              <select
+                id="filterTag"
+                value={filterTag}
+                onChange={handleFilterTag}
+                className='input-element'
+                style={{ color: 'black' }}
+                
+              >
+              <option value="default" hidden>Select One</option>
+              <option value="Work">Work</option>
+              <option value="Education">Education</option>
+              <option value="Health">Health</option>
+              <option value="Finance">Finance</option>
+              <option value="Household">Household</option>
+              <option value="Family">Family</option>
+              <option value="Travel">Travel</option>
+              <option value="Others">Others</option>
+              </select>
+              <label htmlFor='filterPriority'  className='label'>PRIORITY</label>
+            <select
+                id="filterPriority"
+                value={filterPriority}
+                onChange={handleFilterPriority}
+                className='input-element'
+                style={{ color: 'black' }}
+            > 
+            <option value="default" hidden>Select One</option>
+                <option value="low">low</option>
+                <option value="medium">medium</option>
+                <option value="high">high</option>
+            </select>
+            <label  htmlFor='status' className='label'>STATUS</label>
+            <select
+                value={status}
+                onChange={handleStatus}
+                className='input-element'
+                style={{ color: 'black' }}
+                id='status'
+            > 
+            <option value="default" hidden>Select One</option>
+                <option value="pending">pending</option>
+                <option value="completed">completed</option>
+                
+            </select>
+            <button onClick={handleRemoveFilters} className='remove-filters-button'>Remove Filters</button>
+          </div>
+          
         </div>
+        
         <div  className='todo-top-container'>
-          {data.map((item) => (
+        
+          {filteredData.length===0 ? (<div className='no-todos-container'>
+            
+            <img  className='todo-image-1' alt="todo" src="https://img.freepik.com/free-photo/3d-illustration-calendar-with-checkmarks-pen_107791-15855.jpg" />
+          <div>
+            <p className='no-todos-content'>No Todos for this Date: <span className="formatted-date-heading">{formattedDate}</span></p>
+            <h3 style={{fontWeight:"bold",color:"green"}}>Please Try to Add Tasks</h3>
+          </div>
+          </div>):(filteredData.map((item) => (
             <div className='each-todo' key={item._id}>
               <input className="checkbox" type="checkbox" />
-              <p>Task: {item.todo}</p>
+              <p>{item.todo}</p>
               {/* <FaEdit className='icon' title="Edit Task" /> */}
               <button className="icon" title="Edit Task" style={{ color: 'black' }}>📝</button>
-              <button className="icon" title="Delete Task" style={{ color: 'red' }}>🗑️</button>
+              <button  onClick={()=>handleDeleteTodo(item._id)} className="icon" title="Delete Task" style={{ color: 'red' }}>🗑️</button>
             </div>
-          ))}
+          ))) }
+          
         </div>
       </div>
     </div>
