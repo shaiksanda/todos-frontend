aa28d035bdb923636bd9f5bd21fb36dc3560c575
shaiksanda <shaiksandanibasha7@gmail.com>
2024-12-01 16:40:40 +0530

    added Some Features and fixed some Bugs

--- src/Components/Todo/index.js
+++ src/Components/Todo/index.js
@@ -1,9 +1,14 @@
 import { useState,useEffect} from 'react';
 import Calendar from 'react-calendar';
+import Confetti from 'react-confetti';
+
 import Cookies from 'js-cookie';
 import { Oval } from 'react-loader-spinner'
+import Popup from 'reactjs-popup'
+
 import { FcFilledFilter } from "react-icons/fc";
 
+import 'reactjs-popup/dist/index.css'
 import 'react-calendar/dist/Calendar.css';
 
 import "./index.css"
@@ -23,10 +28,36 @@ const Todo = () => {
   const [search, setSearch] = useState('');
   const [status, setStatus] = useState('');
   const [isLoading, setIsLoading] = useState(true)
+  const [taskAdded, setTaskAdded] = useState(false);
+  const [taskUpdated, setTaskUpdated] = useState(false)
+  const [showUpdatedMsg, setShowUpdatedMsg] = useState(false)
+
+  const [showConfetti, setShowConfetti] = useState(false);
+
+
+  
+  useEffect(() => {
+    if (data.length > 0) {
+      const allCompleted = data.every(todo => todo.status === 'completed');
+    
+      if (allCompleted) {
+        setShowConfetti(true);
+  
+        const timer = setTimeout(() => {
+          setShowConfetti(false);
+        }, 5000);
+  
+        return () => clearTimeout(timer);
+      } else {
+        setShowConfetti(false);
+      }
+    }
+  }, [data]);
+  
   
   
 
-  
+
 
   const handleFilterTag=(event)=>{
     setFilterTag(event.target.value)
@@ -38,6 +69,33 @@ const Todo = () => {
 
   const handleSearch=(event)=>{
     setSearch(event.target.value)
+  }
+
+  const handleUpdateTask=async(event,id)=>{
+    event.preventDefault()
+    const url=`https://todos-backend-d9im.onrender.com/todos/${id}`
+    const options = {
+      method:"PUT",
+      headers: {
+        'content-type':"application/json",
+        'Authorization': `Bearer ${Cookies.get('jwt_token')}`, // Authorization header
+      },
+      body:JSON.stringify({
+        todo:todo,
+        priority:priority,
+        tag:tag
+      })
+    }
+    const response=await fetch(url,options)
+    if(response.ok){
+      onSubmitSuccess({message:""})
+      setTaskUpdated(true);
+      setShowUpdatedMsg(true)
+    }
+    else{
+      console.log("Error in updating todo",response.statusText)
+      setTaskUpdated(false)
+    }
   }
 
   const handleStatus=(event)=>{
@@ -76,7 +134,7 @@ const Todo = () => {
     }
 
     getTodos()
-  },[selectedDate,filterPriority,filterTag,status])
+  },[selectedDate,filterPriority,filterTag,status,taskAdded,taskUpdated])
 
   const onSubmitSuccess=(data)=>{
         setMsg(data.message)
@@ -88,6 +146,7 @@ const Todo = () => {
         setTimeout(() => {
           setShowMsg(false);
         }, 5000);
+        setTaskAdded(prev => !prev);
   }
 
   const handleAddTask = async (event) => {
@@ -136,6 +195,46 @@ const Todo = () => {
       console.error('Error adding task:', error);
     }
   };
+
+  const handleCheckboxStatus=async(id,currentStatus)=>{
+    setIsLoading(true)
+    
+    const newStatus = currentStatus === 'completed' ? 'pending' : 'completed';
+    const url=`https://todos-backend-d9im.onrender.com/todos/${id}`
+    const options={
+      method:"PUT",
+      headers: {
+        'content-type': 'application/json',
+        'Authorization': `Bearer ${Cookies.get('jwt_token')}`, // Authorization header
+      },
+      body:JSON.stringify({
+        status: newStatus,
+      })
+    }
+
+
+    const response=await fetch(url,options)
+    const url1='https://todos-backend-d9im.onrender.com/todos'
+    const options1={
+      method:"GET",
+      headers: {
+        'content-type': 'application/json',
+        'Authorization': `Bearer ${Cookies.get('jwt_token')}`, // Authorization header
+      },
+    }
+    const response1=await fetch(url1,options1)
+    const jsonData=await response1.json()
+    if (response.ok){
+      setData(jsonData)
+      setIsLoading(false)
+    }
+    else{
+      setIsLoading(false)
+      console.log("Error in updating status",response.statusText)
+    }
+
+
+  }
   
 
   // Handler for when the selection changes
@@ -154,6 +253,8 @@ const Todo = () => {
   const handleDateChange = (date) => {
     setSelectedDate(date);
   };
+
+  
 
   const formattedDate = `${selectedDate.getDate()}/${selectedDate.toLocaleString('default', { weekday: 'short' })}/${selectedDate.getFullYear()}`;
   const username=Cookies.get('username');
@@ -177,11 +278,47 @@ const Todo = () => {
         },
       }
       const response=await fetch(url,options)
-      if (response.ok){
-        console.log("Todo deleted successfully")
+      const url1=`https://todos-backend-d9im.onrender.com/todos?selectedDate=${selectedDate.toISOString()}`
+      const options1={
+        method:"GET",
+        headers: {
+          'content-type': 'application/json',
+          'Authorization': `Bearer ${Cookies.get('jwt_token')}`,
+        },
+      }
+
+      
+
+      const response1=await fetch(url1,options1)
+      const jsonData1=await response1.json()
+      if (response1.ok){
+        setData(jsonData1)
+        setIsLoading(false)
       }
       else{
-        console.log("Failed to Delete Todo")
+        console.log("Error in fetching data",response.statusText)
+        setIsLoading(false)
+      }
+    
+    }
+
+    const handleDeleteAllTodos=async()=>{
+      const url=`https://todos-backend-d9im.onrender.com/todos?selectedDate=${selectedDate.toISOString()}`
+      const options={
+        method:"DELETE",
+        headers: {
+          'content-type': 'application/json',
+          'Authorization': `Bearer ${Cookies.get('jwt_token')}`, // Authorization header
+        },
+      }
+      const response=await fetch(url,options)
+      if (response.ok){
+        setData([])
+        setIsLoading(false)
+      }
+      else{
+        console.log("Error in deleting data",response.statusText)
+        setIsLoading(false)
       }
     }
     
@@ -190,6 +327,7 @@ const Todo = () => {
   
   return (
     <div className="todos-bg-container">
+
      <div className="todo-top-container">
         <div className="calendar-container">
           <h1> <span style={{ color: 'black' }}>Welcome </span>
@@ -256,6 +394,9 @@ const Todo = () => {
      </div>
 
      <h1 className="fetch-todos-heading">Fetching todos for: <span className="formatted-date-heading">{formattedDate}</span></h1>
+     <div style={{textAlign:'right'}}>
+     <button onClick={handleDeleteAllTodos} className='delete-all-todos-button'>Delete All Todos</button>
+     </div>
      <div className="spinner-container">
      <div className="spinner-container">
       {isLoading && <Oval
@@ -267,6 +408,10 @@ const Todo = () => {
     </div>
     </div>
       <div className='todo-bottom-container'>
+        <div>
+          {showConfetti && <Confetti />}
+        </div>
+  
         <div>
           <h1 className='filter-heading'>Use Filters to Organize Your Data <FcFilledFilter size={"30px"} /></h1>
           
@@ -333,10 +478,81 @@ const Todo = () => {
           </div>
           </div>):(filteredData.map((item) => (
             <div className='each-todo' key={item._id}>
-              <input className="checkbox" type="checkbox" />
+              <input checked={item.status==="completed"}  onChange={()=>handleCheckboxStatus(item._id,item.status)} className="checkbox" type="checkbox" />
               <p>{item.todo}</p>
               {/* <FaEdit className='icon' title="Edit Task" /> */}
-              <button className="icon" title="Edit Task" style={{ color: 'black' }}>📝</button>
+              
+                <Popup contentStyle={{
+                    backgroundColor: 'white',
+                    border: 'none',
+                    borderRadius: '12px',
+                    width: '90%', // Full width for small devices
+                    maxWidth: '400px', // Optional: Limit max width for larger devices
+                    
+  
+                    }}
+                  position="right center" modal
+                   trigger={<button  className="icon" title="Edit Task" style={{ color: 'black' }}>
+                    📝</button>}>
+
+                  {close => (
+                    <div className="update-todo-container">
+                    
+                    <h1 className='update-heading'>Update Your Task</h1>
+
+                    <form onSubmit={(e)=>handleUpdateTask(e,item._id)} id="form" className="task-container" >
+                      <label htmlFor="task" className="label">
+                        TASK
+                      </label>
+                      <input value={todo} onChange={handleTodoChange} id="task" placeholder="Enter the Task Here" className="input-element" type="text" />
+                      <label htmlFor="tag" className='label'>TAG</label>
+                      <select
+                        name="tag"
+                        value={tag}
+                        onChange={handleTagChange}
+                        className='input-element'
+                        style={{ color: 'black' }}
+                        id="tag"
+                        >
+                          <option value="default" hidden>Select One</option>
+                          <option value="Work">Work</option>
+                          <option value="Education">Education</option>
+                          <option value="Health">Health</option>
+                          <option value="Finance">Finance</option>
+                          <option value="Household">Household</option>
+                          <option value="Family">Family</option>
+                          <option value="Travel">Travel</option>
+                          <option value="Others">Others</option>
+                        </select>
+
+                      <label htmlFor="priority" className='label'>PRIORITY</label>
+                      <select
+                        id="priority"
+                        name="priority"
+                        value={priority}
+                        onChange={handlePriorityChange}
+                        className='input-element'
+                        style={{ color: 'black' }}
+                      > 
+                        <option value="default" hidden>Select One</option>
+                        <option value="low">low</option>
+                        <option value="medium">medium</option>
+                        <option value="high">high</option>
+                
+                      </select>
+                      <button type="submit" className="login-button-form">
+                          UPDATE
+                      </button>
+                      {showUpdatedMsg && <p style={{color:"green",fontWeight:"bold"}}>Todo Updated Successfully now close this.</p>}
+                      <button style={{textAlign:"center"}} onClick={close} className='delete-all-todos-button'>Close</button>
+
+                    </form>
+                    <div>   
+                </div>
+            </div>
+          )}
+                </Popup>
+              
               <button  onClick={()=>handleDeleteTodo(item._id)} className="icon" title="Delete Task" style={{ color: 'red' }}>🗑️</button>
             </div>
           ))) }
