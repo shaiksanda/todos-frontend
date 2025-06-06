80493d5cb637fc88b220b780cf6ac41f47cda357
shaiksanda <shaiksandanibasha7@gmail.com>
2025-06-06 12:30:05 +0530

    Added react-toastify for signup component

--- src/Components/Signup/index.js
+++ src/Components/Signup/index.js
@@ -2,31 +2,20 @@ import { useState } from 'react';
 import { Link } from "react-router-dom"
 import { useNavigate } from 'react-router-dom';
 import ClipLoader from "react-spinners/ClipLoader";
+import { toast } from 'react-toastify';
 import 'remixicon/fonts/remixicon.css';
 import "./index.css"
+import { useUserRegisterMutation } from '../../services/todoService';
 const Signup = () => {
     const [showPassword, setShowPassword] = useState(false)
     const [username, setUsername] = useState("")
     const [fullname, setFullname] = useState("")
     const [password, setPassword] = useState("")
-    const [gender, setGender] = useState("Male")
+    const [gender, setGender] = useState("")
+
+    const [userRegister,{isLoading}]=useUserRegisterMutation()
 
     const navigate = useNavigate();
-
-    const [errorMsg, setErrorMsg] = useState("")
-    const [showErrorMsg, setShowErrorMsg] = useState(false)
-
-    const [successMsg, setSuccessMsg] = useState("")
-    const [showSuccessMsg, setShowSuccessMsg] = useState(false)
-
-    const [loading,setLoading]=useState(false)
-
-    if (showErrorMsg) {
-        setTimeout(() => {
-            setShowErrorMsg(false);
-            setErrorMsg('');
-        }, 5000)
-    }
 
     const handleShowPassword = () => {
         setShowPassword(!showPassword)
@@ -43,48 +32,28 @@ const Signup = () => {
     const handlePassword = (event) => {
         setPassword(event.target.value)
     }
-    const handleSubmitSuccess = (message) => {
-        setShowSuccessMsg(true)
-        setSuccessMsg(message)
-        console.log("Registration successful:", message);
-        setTimeout(()=>{
-            navigate("/login")
-        },2000)
+    const handleSubmitSuccess = () => {
+       navigate("/login")
+       toast.success("User Registered Successfully")
     }
 
-    const handleSubmitError = (errorMsg) => {
-        setShowErrorMsg(true)
-        setErrorMsg(errorMsg)
-    }
+    
     const handleSubmit = async (event) => {
         event.preventDefault()
-        if (!username || !fullname || !password) {
-            handleSubmitError("Please fill in all fields.");
+        if (!username || !fullname || !password || !gender) {
+            toast.error("Please fill in all fields.");
             return;
         }
-        setLoading(true)
-
+        
         const userDetails = { username, fullname, password, gender }
-        const url = "https://todos-backend-d9im.onrender.com/register"
-        const options = {
-            method: "POST",
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(userDetails)
+        try{
+            await userRegister(userDetails).unwrap()
+            handleSubmitSuccess()
         }
-
-        const response = await fetch(url, options)
-        const data = await response.json()
-        console.log(data)
-        setLoading(false)
-        if (response.ok) {
-            handleSubmitSuccess(data.message)
-        }
-        else {
-            handleSubmitError(data.message)
-        }
-
+        catch(error){
+            const message=error?.data?.message||error?.error || "User Registration Failed"
+            toast.error(message)
+        } 
     }
 
     const isValid=fullname && username && password && gender
@@ -96,7 +65,7 @@ const Signup = () => {
             <form id="form" onSubmit={handleSubmit} className="form-container">
                 <div className="input-wrapper">
                     <input required name="fullname" id="fullname" onChange={handleFullName} value={fullname}  className="input-element" type="text" />
-                    <label id="fullname" className="label">FULLNAME</label>
+                    <label htmlFor="fullname" className="label">FULLNAME</label>
                 </div>
                 <div className="input-wrapper">
                     <input required name="username" id="username" onChange={handleUsername} value={username} className="input-element" type="text" />
@@ -104,7 +73,7 @@ const Signup = () => {
                 </div>
                 <div>
                     <select  name="gender" id="gender" onChange={handleGender} value={gender} className='dropdown' required style={{ color: gender===""?"gray":"magenta" }}>
-                        <option value="default" hidden>Choose Gender</option>
+                        <option value="" hidden>Choose Gender</option>
                         <option value="male">Male</option>
                         <option value="female">Female</option>
                         <option value="others">Others</option>
@@ -117,19 +86,12 @@ const Signup = () => {
                 <label htmlFor='password' className="label" >PASSWORD</label>
                 {showPassword ? (<i onClick={handleShowPassword} className="ri-eye-line eye"></i>) : (<i onClick={handleShowPassword} className="ri-eye-off-line eye"></i>)}
                 </div>
-                {/* <div className="checkbox-container">
-                    <input name="checkbox" onChange={handleCheckbox} id="checkbox" type="checkbox" className="checkbox" />
-                    <label style={{ cursor: "pointer" }} htmlFor="checkbox" className="label" >Show Password</label>
-                </div> */}
-
-                <button disabled={loading || !isValid} type="submit" className="signup-button-form">
-                {loading ? (<span style={{ display: "flex", alignItems: "center", gap: "8px",justifyContent:"center" }}>
+                <button disabled={isLoading || !isValid} type="submit" className="signup-button-form">
+                {isLoading ? (<span style={{ display: "flex", alignItems: "center", gap: "8px",justifyContent:"center" }}>
                     Processing...
                     <ClipLoader color="#007bff" size={15} />
                 </span>) : ("Sign Up")}
                 </button>
-                {showSuccessMsg && <p className='success-msg'>✅{successMsg} Please Login Now</p>}
-                {showErrorMsg && <p style={{ color: "red", fontWeight: "bold" }}>{errorMsg}</p>}
                 <Link to="/login" style={{ textDecoration: "none" }} className='login-text'>Already signed up? Log in</Link>
             </form>
         </div>
